192dc0a15407c9a86c402a11492ff80f59cfe791
Erinn Gentry <erinn.gentry@my.hamptonu.edu>
2017-06-29 16:41:39 -0400

    Add files via upload

--- box.py
+++ box.py
@@ -2,5 +2,11 @@ def box(width, height):
     for h in range(height):
         print width * '#'
 
+'''
+OR
+
+def orBox(width, height):
+	print ('#' * width + '\n') * height
+'''
 
 box(int(raw_input('width?')), int(raw_input('height?')))

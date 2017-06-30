5ac0bdad03544bf7a60b2c5d6a803ad0af1cadce
Erinn Gentry <erinn.gentry@my.hamptonu.edu>
2017-06-30 15:15:14 -0400

    Add files via upload

---
+++ parrot.py
@@ -0,0 +1,35 @@
+'''x = raw_input("Tell the parrot something:")
+
+def boringParrot(x):
+    print x;
+
+boringParrot(x);
+boringParrot(x[::-1]);
+
+x = raw_input("Pick a number.")
+y = raw_input("Pick another number.")
+def mathParrot(x, y):
+    new_x = int(x);
+    new_y = int(y);
+    print new_x + new_y;
+mathParrot(x,y);
+
+name = raw_input("What\'s your name?")
+age = raw_input("How old are you?")
+
+def friendlyParrot(name, age):
+    print str("Hello, " + name + "." + " You are " + age + "years old.")
+
+friendlyParrot(name, age);
+
+x = raw_input("What\'s your favorite movie?")
+y = raw_input("What\'s your favorite food?")
+z = raw_input("What\'s your favorite song?")
+
+def favesParrot(x,y,z):
+    print "I love " + x + " too! I love " + y + " too! I love " + z + " too."
+
+favesParrot(x,y,z);
+'''
+
+

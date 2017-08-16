887840fe0c02913c8ff49dada0103fb57a2b11c0
Erinn Gentry <erinn.gentry@my.hamptonu.edu>
2017-08-15 23:19:18 -0500

    Add files via upload

--- finalproject/main.py
+++ finalproject/main.py
@@ -2,12 +2,14 @@ import webapp2
 import jinja2
 import os
 from google.appengine.ext import ndb
+from datetime import datetime
 
 jinja_environment = jinja2.Environment(
 	loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 class Student(ndb.Model):
 	name = ndb.StringProperty()
+	timestamp = ndb.DateTimeProperty()
 	uni = ndb.StringProperty()
 	study = ndb.StringProperty()
 	hobby = ndb.StringProperty()
@@ -22,17 +24,30 @@ class MainHandler(webapp2.RequestHandler):
 		uni = self.request.get('uni')
 		study = self.request.get('study')
 		hobby = self.request.get('hobby')
-		data_model = Student(name = name, uni = uni, study = study, hobby = hobby)
-		#data_key = data_model.put()
-		
-		data_query = data_model.query()
-		list_of_data = data_query.fetch(limit=1)
+		timestamp = datetime.now()
+		data_model = Student(name = name, uni = uni, study = study, hobby = hobby, timestamp = timestamp)
+		data_key = data_model.put()
+
+class OneUserHandler(webapp2.RequestHandler):
+	def get(self):
+		# name = self.request.get('name')
+		# uni = self.request.get('uni')
+		# study = self.request.get('study')
+		# hobby = self.request.get('hobby')
+		# data_model = Student(namem = name, uni = uni, study = study, hobby = hobby)
+		data_key = Student.query().order(-Student.timestamp)
+		data_name = data_key.get().name
+		data_uni = data_key.get().uni
+		data_study = data_key.get().study
+		data_hobby = data_key.get().hobby
+		#data_query = Student.query().fetch(limit=1)
+		#list_of_data = data_query.fetch(limit=1)
 		template = jinja_environment.get_template('oneu.html')
 		self.response.write(template.render({
-		'name' : name,
-		'uni' : uni,
-		'study' : study, 
-		'hobby' : hobby,
+			'name' : data_name,
+			'uni' : data_uni,
+			'study' : data_study,
+			'hobby' : data_hobby
 		}))
 
 class UserOutputHandler(webapp2.RequestHandler):
@@ -44,6 +59,6 @@ class UserOutputHandler(webapp2.RequestHandler):
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
-    # ('/oneu', OneUserHandler),
+    ('/oneu', OneUserHandler),
     ('/dout', UserOutputHandler),
 ], debug=True)
